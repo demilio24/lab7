da767f7d1800303896c2b0d19b57bc76c0c163cb
Emilio Arias <113644507+demilio24@users.noreply.github.com>
2023-03-21 23:58:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function init(){
-//add your javascrip between these two lines of code
+function init() {
+  const alertDiv = document.querySelector('#alert-me');
+  const inputField = document.querySelector('#input-field');
+  const submitButton = document.querySelector('#submit-btn');
+  const h2 = document.querySelector('h2');
+
+
+  alertDiv.addEventListener('click', function() {
+    const text = inputField.value;
+    const message = `Emilio Arias: ${text}`;
+    alert(message);
+  });
+
+
+  submitButton.addEventListener('click', function() {
+    // Show an Alert box with the contents of the text field in the Alert box
+    const text = inputField.value;
+    const message = `Emilio Arias: ${text}`;
+    alert(message);
+
  
-
-
-
-
-
-
+    h2.textContent = text;
+  });
+}
 
 window.addEventListener('load', init);
